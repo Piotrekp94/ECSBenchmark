cedde7d1c2719ea1c81f6a1fb6a6f3f47acff19f
piotr <piotrekp94@gmail.com>
2021-01-05 12:47:30 +0100

    INIT

---
+++ Assets/MbSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class MbSpawner : MonoBehaviour
+{
+    public GameObject go;
+    public int counter = 0;
+    public Text text; 
+    private void Update()
+     {
+         for (var i = 0; i < 10; i++)
+         {
+             var position = new Vector3(Random.Range(-160.0f, 160.0f), 0, Random.Range(-100.0f, 100.0f));
+             Instantiate(go, position, Quaternion.identity);
+             counter++;
+         }
+
+         text.text = counter.ToString();
+     }
+ }
