07ab78482ae0a7eb26f3c7baaf0daa42e36203b2
piotr <piotrekp94@gmail.com>
2021-01-10 12:21:59 +0100

    Change ComponentSystem to SystemBase

--- Assets/Scripts/WaveJobSystem.cs
+++ Assets/Scripts/WaveJobSystem.cs
@@ -2,29 +2,31 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-public class WaveJobSystem : ComponentSystem
+public class WaveJobSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        Entities.ForEach((ref Translation t, ref WaveData waveData) =>
+        var elapsedTime = (float) Time.ElapsedTime;
+        Entities.ForEach((ref Translation translation, ref WaveData waveData) =>
         {
-            var zPosition = waveData.amplitude * getFunctionBasedOnDesiredFunction(waveData, t);
-            t.Value = new float3(t.Value.x, zPosition, t.Value.z);
-        });
+            var zPosition = waveData.amplitude * getFunctionBasedOnDesiredFunction(waveData, translation, elapsedTime);
+            translation.Value = new float3(translation.Value.x, zPosition, translation.Value.z);
+        }).Run();
     }
 
-    private float getFunctionBasedOnDesiredFunction(WaveData waveData, Translation t)
+    private static float getFunctionBasedOnDesiredFunction(WaveData waveData, Translation translation,
+        float elapsedTime)
     {
         if (waveData.desiredFunctionEnum == DesiredFunctionEnum.Sin)
-            return math.sin((float) Time.ElapsedTime * waveData.speed + t.Value.x * waveData.xOffset +
-                            t.Value.z * waveData.zOffset);
+            return math.sin(elapsedTime * waveData.speed + translation.Value.x * waveData.xOffset +
+                            translation.Value.z * waveData.zOffset);
         if (waveData.desiredFunctionEnum == DesiredFunctionEnum.SquareWave)
-            return math.sign(math.sin((float) Time.ElapsedTime * waveData.speed + t.Value.x * waveData.xOffset +
-                                      t.Value.z * waveData.zOffset));
+            return math.sign(math.sin(elapsedTime * waveData.speed + translation.Value.x * waveData.xOffset +
+                                      translation.Value.z * waveData.zOffset));
 
-        return (float) (Time.ElapsedTime * waveData.speed / 10 + t.Value.x * waveData.xOffset +
-            t.Value.z * waveData.zOffset - math.floor(t.Value.x * waveData.xOffset +
-                                                      t.Value.z * waveData.zOffset +
-                                                      Time.ElapsedTime * waveData.speed / 10 + 0.5f));
+        return elapsedTime * waveData.speed / 10 + translation.Value.x * waveData.xOffset +
+            translation.Value.z * waveData.zOffset - math.floor(translation.Value.x * waveData.xOffset +
+                                                                translation.Value.z * waveData.zOffset +
+                                                                elapsedTime * waveData.speed / 10 + 0.5f);
     }
 }
